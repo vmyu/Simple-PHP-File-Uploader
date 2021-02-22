307f440e86954656c4df08ffe889c3d14857a91a
Vince Y <vince.m.yu@gmail.com>
2021-02-22 14:10:04 -0500

    added view file directory support

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <html>
 <body>
 
-Version 1 - Dev environment
+Version 1 - File listing update
 
+<h3>Existing files:</h3>
+
+<?php
+$directory = './uploads';
+$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+
+foreach($scanned_directory as $file){
+	echo "<a href='./uploads/".$file."'>".$file.'</a><br>';
+
+}
+?>
+
+<h3>Upload New File</h3>
 <form action = "upload.php" method = "post" enctype ="multipart/form-data">
 
 Select file to upload:
 <input type = "file" name ="filetoupload" id = "filetoupload">
-<input type ="submit" value="Upload Image" name="submit">
+<input type ="submit" value="Upload File" name="submit">
 
 
 </form>
